a61c4c8e2f8f464e2d90ad92c7cc6999523113e4
Cassidy Pignatello <cassidy.pignatello@gmail.com>
2023-07-13 16:04:14 +0700

    adds types

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+interface Product {
+  id: number
+  title: string
+  price: number
+}
+
+interface CartItem extends Product {
+  quantity: number
+}
+
+interface State {
+  products: Product[]
+  cart: CartItem[]
+}
+
+export {}
