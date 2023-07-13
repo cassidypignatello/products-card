2cc38d0e8350533e329eee5c9c93241c44e16ada
Cassidy Pignatello <cassidy.pignatello@gmail.com>
2023-07-13 16:06:53 +0700

    adds mock data

---
+++ src/mockData.ts
@@ -0,0 +1,10 @@
+// mockData.ts
+import { Product } from './types';
+
+export const products: Product[] = [
+  { id: 1, title: 'Shampoo', price: 100 },
+  { id: 2, title: 'Conditioner', price: 200 },
+  { id: 3, title: 'Toothpaste', price: 300 },
+  { id: 4, title: 'Mouthwash', price: 400 },
+  { id: 5, title: 'Beer', price: 500 },
+];
